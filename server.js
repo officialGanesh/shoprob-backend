1109c171b78ba33d32cc5366bc6256f083918586
Ganesh <ganeshofficial2795@gmail.com>
2025-04-30 20:24:29 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,22 @@
+import express from "express"
+import { config } from "dotenv"
+
+config()
+
+const PORT = process.env.PORT || 5000
+
+const app = express()
+
+
+
+
+
+
+
+
+
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+})
